44f5298113208199f1bf6f217e832288ce43465c
Mukesh Praveen S <Mukesh Praveen S@MUKESH>
2025-09-22 20:04:52 +0530

    BikeAndCar

--- ABCD2Framework/src/test/java/bike/Ktm.java
+++ ABCD2Framework/src/test/java/bike/Ktm.java
@@ -3,7 +3,8 @@ package bike;
 public class Ktm {
 
 	public static void main(String[] args) {
-		System.out.println("Broom broom");
+		System.out.println("Yamaha FZ V3 is better then KTM");
+		System.out.println("Yamaha FZ V3 is better than pulsar");
 	}
 
 }
--- ABCD2Framework/src/test/java/car/BMW.java
+++ ABCD2Framework/src/test/java/car/BMW.java
@@ -3,7 +3,8 @@ package car;
 public class BMW {
 
 	public static void main(String[] args) {
-		System.out.println("Broom Broom Broom");
+		System.out.println("BMW stands for Bayerische Motoren Werke in german");
+		System.out.println("BMW stands for  Bavarian Motor Works");
 	}
 
 }
